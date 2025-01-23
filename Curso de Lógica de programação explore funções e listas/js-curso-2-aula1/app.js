21511d95e178a79ba4d51c12723c9593ea90249b
camlopes <camalopes.cat@gmail.com>
2025-01-23 10:00:39 -0300

    Melhoria feita no codigo para evitar repeticoes

--- Curso de Lógica de programação explore funções e listas/js-curso-2-aula1/app.js
+++ Curso de Lógica de programação explore funções e listas/js-curso-2-aula1/app.js
@@ -1,9 +1,12 @@
-//Seleciona o h1 no html e muda seu conteudo
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do numero secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+//Chamando a funcao 
+exibirTextoNaTela('h1', 'Jogo do numero secreto');
+exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10');
 
 function verificarChute() {
     console.log('o botao foi clicado');
