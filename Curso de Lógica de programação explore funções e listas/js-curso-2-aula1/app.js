c59a306ec5e20e855df447dd48be80b600ab67e1
camlopes <camalopes.cat@gmail.com>
2025-01-23 11:11:16 -0300

    Desenvolvimento da funcao que verifica o numero adivinhado

--- Curso de Lógica de programação explore funções e listas/js-curso-2-aula1/app.js
+++ Curso de Lógica de programação explore funções e listas/js-curso-2-aula1/app.js
@@ -1,4 +1,7 @@
 
+let numeroSecreto = gerarNumeroAleatorio();
+
+//Funcao que tem parametros e sem retorno de informacao
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -8,7 +11,14 @@ function exibirTextoNaTela(tag, texto) {
 exibirTextoNaTela('h1', 'Jogo do numero secreto');
 exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10');
 
+//Funcao sem parametro e sem retorno
 function verificarChute() {
-    console.log('o botao foi clicado');
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
 }
+
+//Funcao sem parametros mas com retorno de informacao
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+} 
 
