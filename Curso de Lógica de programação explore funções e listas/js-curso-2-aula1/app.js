3c7d445dc936fbad42fc3917dd94379de3bfa462
camlopes <camalopes.cat@gmail.com>
2025-01-23 12:22:10 -0300

    Desenvolvendo a funcao verificarChute
    Foi implementado a logica para verificar se o chute esta correto

--- Curso de Lógica de programação explore funções e listas/js-curso-2-aula1/app.js
+++ Curso de Lógica de programação explore funções e listas/js-curso-2-aula1/app.js
@@ -1,20 +1,8 @@
 
-let numeroSecreto = gerarNumeroAleatorio();
-
 //Funcao que tem parametros e sem retorno de informacao
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-}
-
-//Chamando a funcao 
-exibirTextoNaTela('h1', 'Jogo do numero secreto');
-exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10');
-
-//Funcao sem parametro e sem retorno
-function verificarChute() {
-    let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
 }
 
 //Funcao sem parametros mas com retorno de informacao
@@ -22,3 +10,23 @@ function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
 } 
 
+//Funcao sem parametro e sem retorno
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', 'Voce descobriu o numero secreto!');
+    } else if (chute > numeroSecreto) {
+        exibirTextoNaTela('p', 'O numero secreto é menor');
+    } else {
+        exibirTextoNaTela('p', 'O numero secreto é maior');
+    }
+}
+
+
+let numeroSecreto = gerarNumeroAleatorio();
+
+//Chamando a funcao 
+exibirTextoNaTela('h1', 'Jogo do numero secreto');
+exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10');
