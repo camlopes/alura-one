34cf57ca028c59c217082a9790e8a54ed45e6f66
camlopes <camalopes.cat@gmail.com>
2025-02-04 12:09:11 -0300

    Desenvolver funcoes
    funcoes desenvolvidas: adicionarAmigo(), limparCampo(), atualizarListaDeAmigos(), sortearAmigo()

--- Challenge amigo secreto/challenge-amigo-secreto_pt-main/app.js
+++ Challenge amigo secreto/challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,40 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+function adicionarAmigo() {
+    let amigo = document.querySelector('input').value;
+
+    if (amigo == '') {
+        alert('Por favor, insira um nome.');
+    } else {
+        amigos.push(amigo);
+        limparCampo();
+        atualizarListaDeAmigos();
+    }
+}
+
+function limparCampo() {
+    let amigo = document.querySelector('input');
+    amigo.value = '';
+}
+
+function atualizarListaDeAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = "";
+
+    for (let index = 0; index < amigos.length; index++) {
+        let li = document.createElement('li');
+        let nomeAmigo = document.createTextNode(amigos[index]);
+        li.appendChild(nomeAmigo);
+        listaAmigos.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    let numeroEscolhido = parseInt(Math.random() * amigos.length);
+    let amigoSorteado = amigos[numeroEscolhido];    
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = amigoSorteado;
+
+}
+
+
+let amigos = [];
